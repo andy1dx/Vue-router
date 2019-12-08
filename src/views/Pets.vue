104a3d773b313887482c8092ca060c2b7f19bf38
Andy william <andy.william@bit-okutama.jp>
2019-12-08 21:01:45 +0900

    Commit Learning Vue router and making component

---
+++ src/views/Pets.vue
@@ -0,0 +1,30 @@
+<template>
+  <div>
+    <h1>{{ animal.name }} ({{ $route.params.species }})</h1>
+    <p>AGE : {{ animal.age }}</p>
+    <p>BREED : {{ animal.breed }}</p>
+  </div>
+</template>
+
+<script>
+import { mapState } from 'vuex'
+
+export default {
+  data () {
+    return {
+      animal: {}
+    }
+  },
+  computed: {
+    ...mapState([
+      'cats',
+      'dogs'
+    ])
+  },
+  mounted () {
+    const animal = this[this.$route.params.species][this.$route.params.id]
+    console.log(animal)
+    this.animal = animal
+  }
+}
+</script>
